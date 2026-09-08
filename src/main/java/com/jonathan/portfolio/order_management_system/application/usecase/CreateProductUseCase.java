5868908fd44a3604c8c0625daa08128346c1bc97
Dav-looper <jonathan.torres.lpz@gmail.com>
2026-09-08 16:59:40 -0500

    feature(KAN-10): Define CreateProductUseCase and ProductRepository

--- src/main/java/com/jonathan/portfolio/order_management_system/application/usecase/CreateProductUseCase.java
+++ src/main/java/com/jonathan/portfolio/order_management_system/application/usecase/CreateProductUseCase.java
@@ -1,16 +1,29 @@
 package com.jonathan.portfolio.order_management_system.application.usecase;
 
+import com.jonathan.portfolio.order_management_system.domain.model.Money;
 import com.jonathan.portfolio.order_management_system.domain.ports.ProductPort;
 import com.jonathan.portfolio.order_management_system.domain.model.Product;
+import com.jonathan.portfolio.order_management_system.infrastructure.dtos.RequestProductDto;
+import com.jonathan.portfolio.order_management_system.infrastructure.dtos.ResponseProductDto;
+import com.jonathan.portfolio.order_management_system.infrastructure.mappers.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateProductUseCase {
     private ProductPort productPort;
+    private ProductMapper productMapper;
 
-    public CreateProductUseCase(ProductPort productPort) {
+    @Autowired
+    public CreateProductUseCase(ProductPort productPort, ProductMapper productMapper) {
         this.productPort = productPort;
+        this.productMapper = productMapper;
     }
 
-    public Product execute() {
-        return null;
+    public ResponseProductDto execute(RequestProductDto requestProductDto) {
+        Money productPrice = new Money(requestProductDto.amount(), requestProductDto.currency());
+        Product product = new Product(requestProductDto.name(), productPrice);
+        Product productSaved = productPort.save(product);
+        return productMapper.domainEntityToDto(productSaved);
     };
 }
--- src/main/java/com/jonathan/portfolio/order_management_system/infrastructure/adapters/ProductAdapter.java
+++ src/main/java/com/jonathan/portfolio/order_management_system/infrastructure/adapters/ProductAdapter.java
@@ -3,17 +3,26 @@ package com.jonathan.portfolio.order_management_system.infrastructure.adapters;
 import com.jonathan.portfolio.order_management_system.domain.ports.ProductPort;
 import com.jonathan.portfolio.order_management_system.domain.model.Product;
 import com.jonathan.portfolio.order_management_system.infrastructure.entities.ProductEntity;
+import com.jonathan.portfolio.order_management_system.infrastructure.mappers.ProductMapper;
 import com.jonathan.portfolio.order_management_system.infrastructure.repositories.JpaProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ProductAdapter implements ProductPort {
+    private final ProductMapper productMapper;
     private JpaProductRepository jpaProductRepository;
 
-    public ProductAdapter(JpaProductRepository jpaProductRepository) {
+    @Autowired
+    public ProductAdapter(JpaProductRepository jpaProductRepository, ProductMapper productMapper) {
         this.jpaProductRepository = jpaProductRepository;
+        this.productMapper = productMapper;
     }
 
     @Override
-    public ProductEntity save(Product product) {
-        return null;
+    public Product save(Product product) {
+        ProductEntity productJpaEntity = productMapper.domainEntityToJpaEntity(product);
+        ProductEntity savedProductEntity = jpaProductRepository.save(productJpaEntity);
+        return productMapper.jpaEntityToDomainEntity(savedProductEntity);
     }
 }
